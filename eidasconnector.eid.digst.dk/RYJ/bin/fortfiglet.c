74b223ac57f99d67c5d10cf15ffeebeb39d60de6
Ryszard W. Czekaj <rychoo@CzeKaj.com.PL>
2025-02-14 22:49:45 +0100

    procedury europejskie w dobrym SStYlu od/dla Walentynek i przyjaciół z katalogów *.DK && *.NL 8-[16]

---
+++ eidasconnector.eid.digst.dk/RYJ/bin/fortfiglet.c
@@ -0,0 +1,143 @@
+﻿
+/* 
+ * Copyright (c) 2004 mgr ... Ryszard Wiesl/aw Czekaj <rycho@Czekaj.net.PL>
+ * All rights reserved.
+ * $ Utor vi,      uti FreeBSD
+ * # _________^-^__
+ * # bofh@RYCHo.oRG
+ * # ~~~~~~~~~ v ~~
+ * This code is in the public domain for <a href="https://www.u2.com/">U2</a>`.
+ * You must have (figlet && fortune) installed in path.
+
+(c) 2023 I'm commercial! All from this code 33% income for me ;)
++48 882723907
+(R) 2025 ABUSE@CZEKAJ.COM.pl
+
+37w2SC3qoDvtXBC5Be1e2CbKPx5gwRDhDv
+3PCFHnY1ujpCchFNSBAeaECZ8rGyScoRLT
+
+PLN:
+Beneficjent: Ryszard Czekaj
+IBAN: LT773250049791461533
+Kod BIC: REVOLT21
+Kod BIC banku pośredniczącego: BARCGB22
+Adres beneficjenta: Zwyciestwa pl., 8/24, 44-300, Wodzislaw Slaski, Poland
+Bank lub instytucja płatnicza: Revolut Bank UAB
+Adres banku: Konstitucijos ave. 21B, 08130, Vilnius, Lithuania
+
+DKK:
+Beneficjent: Ryszard Czekaj
+IBAN: LT773250049791461533
+Kod BIC: REVOLT21
+Kod BIC banku pośredniczącego: BARCGB22
+Adres beneficjenta: Zwyciestwa pl., 8/24, 44-300, Wodzislaw Slaski, Poland
+Bank lub instytucja płatnicza: Revolut Bank UAB
+Adres banku: Konstitucijos ave. 21B, 08130, Vilnius, Lithuania
+
+Local EUR:
+Beneficjent: Ryszard Czekaj
+IBAN: LT773250049791461533
+Kod BIC: REVOLT21
+Adres beneficjenta: Zwyciestwa pl., 8/24, 44-300, Wodzislaw Slaski, Poland
+Bank lub instytucja płatnicza: Revolut Bank UAB
+Adres banku: Konstitucijos ave. 21B, 08130, Vilnius, Lithuania
+
+International EUR:
+Beneficjent: Ryszard Czekaj
+IBAN: LT773250049791461533
+Kod BIC: REVOLT21
+Kod BIC banku pośredniczącego: CHASDEFX
+Adres beneficjenta: Zwyciestwa pl., 8/24, 44-300, Wodzislaw Slaski, Poland
+Bank lub instytucja płatnicza: Revolut Bank UAB
+Adres banku: Konstitucijos ave. 21B, 08130, Vilnius, Lithuania
+
+International USD:
+Beneficjent: Ryszard Czekaj
+IBAN: LT773250049791461533
+Kod BIC: REVOLT21
+Kod BIC banku pośredniczącego: CHASDEFX
+Adres beneficjenta: Zwyciestwa pl., 8/24, 44-300, Wodzislaw Slaski, Poland
+Bank lub instytucja płatnicza: Revolut Bank UAB
+Adres banku: Konstitucijos ave. 21B, 08130, Vilnius, Lithuania
+
+Local USD:
+Beneficjent: Ryszard Czekaj
+IBAN: LT773250049791461533
+Kod BIC: REVOLT21
+Adres beneficjenta: Zwyciestwa pl., 8/24, 44-300, Wodzislaw Slaski, Poland
+Bank lub instytucja płatnicza: Revolut Bank UAB
+Adres banku: Konstitucijos ave. 21B, 08130, Vilnius, Lithuania
+
+CZK:
+Beneficjent: Ryszard Czekaj
+IBAN: LT773250049791461533
+Kod BIC: REVOLT21
+Kod BIC banku pośredniczącego: BARCGB22
+Adres beneficjenta: Zwyciestwa pl., 8/24, 44-300, Wodzislaw Slaski, Poland
+Bank lub instytucja płatnicza: Revolut Bank UAB
+Adres banku: Konstitucijos ave. 21B, 08130, Vilnius, Lithuania
+
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static const char rcsid[] = "$Id: fortfiglet.c,v 1.5 2025/02/12 06:09:23 $";
+
+int main(int argc, char **argv)
+{
+    char litery[] = {34, 36, 37, 38, 42, 43, 45, 47, 48, 49, 50, 51,  52,
+    53, 54, 55, 56, 57, 58, 59, 61, 63, 64, 65, 66, 67, 68, 69,  70,  71,
+    72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86,  87,  88,
+    89, 90, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103,  104,  105,  106,
+    107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
+    121, 122, 196, 214, 220, 223, 228, 246, 252};
+    register unsigned litera, max;
+    char buf[255], linia[255];
+    FILE *pipe, *sig;
+
+    max = sizeof litery;
+     srandomdev(); 
+    srandom(time() % max);
+    srandom(random());
+    litera = random() % max;
+    /* WSzW[16] w KATOWICACH 2025
+    If not working for your UNi*,
+    try to read one char from /dev/random
+    and cast it to the *int*
+    8-) The Artist === The Teacher := @<sp9ryc>#792466%2025$++
+    */
+
+    sprintf(buf, "echo '%c' | figlet -f eftiwall", litery[litera]);
+    pipe = popen(buf, "r");
+    if (!pipe)
+    {
+        fprintf(stderr, "Stream error -> figlet -f eftiwall\n");
+        exit(EXIT_FAILURE);
+    }
+
+    sprintf(buf, "fortune");
+    sig = popen(buf, "r");
+    if (!sig)
+    {
+        fprintf(stderr, "Stream error -> fortune\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (max = 0; max < 5; ++max)
+    {
+        if (fgets(linia, sizeof linia, pipe))
+        {
+            litera = strlen(linia);
+            linia[--litera] = '\0';
+            fprintf(stdout, "%s", linia);
+            if (fgets(buf, sizeof buf, sig))
+                fprintf(stdout, "%s", buf);
+            else
+                fprintf(stdout, "\n");
+        }
+    }
+    pclose(pipe);
+    pclose(sig);
+    exit(EXIT_SUCCESS);
+}
