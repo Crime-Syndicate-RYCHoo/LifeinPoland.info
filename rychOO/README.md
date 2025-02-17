9b6afaf9084f88fdd29e1ded6ece2b3d102a8550
Ryszard W. Czekaj <rychoo@CzeKaj.com.PL>
2025-02-17 16:49:04 +0100

    Europa.eu działa na niciach z piątku, garnitury powinny być gotowe w odpowiednim Cza$ie! CzaSię napić!

---
+++ rychOO/README.md
@@ -0,0 +1,9 @@
+# rychOO
+ SP9RYC.EDU.pl
+---
+
+               BOFH excuse #15:
+     ...       
+    (o -)      temporary routing anomaly
+ooO--(_)--Ooo- 
+sp9rycHO
---
+++ rychOO/bofh.org.pl/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtm
+l1-transitional.dtd">
+<!-- saved from url=(0042)http://bonifacy.czekaj.com.pl/bofh.org.pl/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Refresh" content="321; URL=http://www.CzeKaj.com.pl/bofh.org.pl/"><title>
+fortfiglet with RYCHoo@freeshell.net 8-)
+</title>
+<script type="text/javascript">
+<!--
+if (self.parent.frames.length != 0){
+        self.parent.location = document.location;
+}
+//--></script>
+</head>
+<body>
+
+
+<table align="center"><tbody><tr><td>
+<pre>     ___       BOOK OF PSALMS 105
+   .'_#_`.     A confession of the manifold sins and ingratitudes of the Israelites.
+   |[o o]|     
+ooO--(_)--Ooo- 105:32. They provoked him also at the waters of contradiction:  and
+Moses was afflicted for their sakes:
+105:33. Because they exasperated his spirit.  And he distinguished with
+his lips.
+He distinguished with his lips. . .Moses, by occasion of the people's
+rebellion and incredulity, was guilty of distinguishing with his lips;
+when, instead of speaking to the rock, as God had commanded, he said to
+the people, with a certain hesitation in his faith, Hear ye, rebellious
+and incredulous:  Can we from this rock bring out water for you?  Num.
+20.10.
+
+</pre>
+</td></tr></tbody></table>
+<center>sp9rycHO</center>
+<?php
+phpinfo();
+?>
+</body></html>
